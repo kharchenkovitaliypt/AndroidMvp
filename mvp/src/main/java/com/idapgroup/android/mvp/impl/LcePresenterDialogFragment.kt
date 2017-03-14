51f7e149419507cd9932bb14ba43a2a1a2bd7e04
kharchenkovitaliypt <kharchenkovitaliypt@gmail.com>
2017-03-14 17:40:32 +0200

    Added LcePresenterDialogFragment

---
+++ mvp/src/main/java/com/idapgroup/android/mvp/impl/LcePresenterDialogFragment.kt
@@ -0,0 +1,45 @@
+package com.idapgroup.android.mvp.impl
+
+import android.os.Bundle
+import android.support.annotation.CallSuper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.idapgroup.android.mvp.LceView
+import com.idapgroup.android.mvp.MvpPresenter
+
+/** Fragment for displaying loading states(load, content, error)  */
+abstract class LcePresenterDialogFragment<V, out P : MvpPresenter<V>> :
+        BasePresenterDialogFragment<V, P>(),
+        LceView{
+
+    protected val lceViewHandler = LceViewHandler()
+
+    open val lceViewCreator: LceViewCreator = DefaultLceViewCreator { inflater, container ->
+        onCreateContentView(inflater, container)
+    }
+
+    abstract fun onCreateContentView(inflater: LayoutInflater, container: ViewGroup): View
+
+    override fun onCreateView(inflater: LayoutInflater, rootContainer: ViewGroup?, savedInstanceState: Bundle?): View {
+        return lceViewHandler.createAndInitView(inflater, rootContainer, lceViewCreator)
+    }
+
+    @CallSuper
+    override fun onDestroyView() {
+        super.onDestroyView()
+        lceViewHandler.resetView()
+    }
+
+    override fun showLoad() {
+        lceViewHandler.showLoad()
+    }
+
+    override fun showContent() {
+        lceViewHandler.showContent()
+    }
+
+    override fun showError(errorMessage: String, retry : (() -> Unit)?) {
+        lceViewHandler.showError(errorMessage, retry)
+    }
+}
